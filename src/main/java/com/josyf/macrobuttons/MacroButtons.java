94f1874c413d7a42ee65d1609ed7ea0d3eed3920
Joseph Garcia <46433893+free-range-chicken@users.noreply.github.com>
2020-11-03 17:37:26 -0600

    project setup

---
+++ src/main/java/com/josyf/macrobuttons/MacroButtons.java
@@ -0,0 +1,13 @@
+package com.josyf.macrobuttons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MacroButtons implements ModInitializer {
+
+    public static final String MOD_ID = "mgbuttons";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
